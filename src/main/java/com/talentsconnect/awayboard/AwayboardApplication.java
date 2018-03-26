779fb54069b0a828379feb9dbef601e9e6a39080
rmanjaramkar <rmanjaramkar@shutterfly.com>
2018-03-26 22:28:37 +0530

    First Commit

---
+++ src/main/java/com/talentsconnect/awayboard/AwayboardApplication.java
@@ -0,0 +1,12 @@
+package com.talentsconnect.awayboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwayboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwayboardApplication.class, args);
+	}
+}
